ce7c743bb548ffeb444b98bd3d4dd9e36ac7c032
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-28 19:48:09 +0200

    Create database directory

--- res/strings.py
+++ res/strings.py
@@ -1,8 +1,9 @@
-from utils import path_in_current_dir
+import os
+
+from utils import path_in_current_dir, path_in_appdata_dir
 
 APP_NAME = "Quick clipboard"
-
 ICON_PATH = path_in_current_dir("imgs/clipboard.png")
-
-DATABASE_PATH = path_in_current_dir("texts.db")
+APPDATA_DIR = path_in_appdata_dir("quickclipboard")
+DATABASE_DIR = os.path.join(APPDATA_DIR, "texts.db")
 ADD_NEW_TEXT_DIALOG_TITE = "Add a new text"
