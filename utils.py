0c8c521656098cc579367813cd66bd74e9cceb94
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-28 19:47:39 +0200

    Add cross-platform app data dir

--- utils.py
+++ utils.py
@@ -1,4 +1,17 @@
 import os
+import platform
+
+
+def path_in_appdata_dir(path):
+    p = None
+    match platform.system():
+        case "Linux":
+            p = os.path.expanduser("~/.local/share")
+        case "Windows":
+            p = os.getenv("LOCALAPPDATA")
+        case "Darwin":
+            p = os.path.expanduser("~/Library/Application Support")
+    return os.path.join(p, path)
 
 
 def path_in_current_dir(path):
