0a4591fa23adf99c9859b8096db1763bdd0090c2
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-28 20:45:02 +0200

    Extract app ID into a global constant

--- res/strings.py
+++ res/strings.py
@@ -7,3 +7,4 @@ ICON_PATH = path_in_current_dir("imgs/clipboard.png")
 APPDATA_DIR = path_in_appdata_dir("quickclipboard")
 DATABASE_DIR = os.path.join(APPDATA_DIR, "texts.db")
 ADD_NEW_TEXT_DIALOG_TITE = "Add a new text"
+APP_ID = 'com.attai.youssef.quickclipboard'
