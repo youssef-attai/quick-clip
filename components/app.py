02590db1cd549b94f6bda98fd7fa8ced18f48c41
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-30 00:51:50 +0200

    Refactor

--- components/app.py
+++ components/app.py
@@ -12,7 +12,7 @@ from model.text import Text
 class App(QApplication):
     def __init__(self, argv, repository):
         super().__init__(argv)
-        self.repository = repository
+        self.text_repository = repository
         self.icon = QIcon(res.strings.ICON_PATH)
         self.setWindowIcon(self.icon)
         self.setApplicationName(res.strings.APP_NAME)
@@ -24,7 +24,7 @@ class App(QApplication):
         if dialog.exec():
             title = dialog.title.text()
             content = dialog.content.text()
-            self.repository.add_new_text(Text(title, content))
+            self.text_repository.add_new_text(Text(title, content))
             self.recreate_menu()
         else:
             pass
@@ -32,11 +32,11 @@ class App(QApplication):
     def recreate_menu(self):
         self.menu.clear()
 
-        texts = self.repository.get_all_texts()
+        texts = self.text_repository.get_all_texts()
 
-        for item in texts:
-            action = self.menu.addAction(item.title)
-            action.triggered.connect(partial(lambda tc: self.clipboard().setText(tc), item.content))
+        for text in texts:
+            action = self.menu.addAction(text.title)
+            action.triggered.connect(partial(lambda tc: self.clipboard().setText(tc), text.content))
 
         self.menu.addSeparator()
 
@@ -45,13 +45,13 @@ class App(QApplication):
 
         delete_text_menu = self.menu.addMenu("Delete text")
 
-        for item in texts:
-            action = delete_text_menu.addAction(item.title)
-            action.triggered.connect(partial(lambda tt: self.remove_text(tt), item.title))
+        for text in texts:
+            action = delete_text_menu.addAction(text.title)
+            action.triggered.connect(partial(lambda tt: self.remove_text(tt), text.title))
 
         quit_action = self.menu.addAction("Quit")
         quit_action.triggered.connect(lambda: sys.exit())
 
     def remove_text(self, title):
-        self.repository.remove_text(title)
+        self.text_repository.remove_text(title)
         self.recreate_menu()
