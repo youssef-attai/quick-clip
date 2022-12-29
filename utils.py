08100bd143512fe2aa691394233aedae132b32e2
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-30 01:22:30 +0200

    Determine if application is a script file or frozen executable

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import sys
 
 
 def path_in_appdata_dir(path):
@@ -15,4 +16,12 @@ def path_in_appdata_dir(path):
 
 
 def path_in_current_dir(path):
-    return os.path.join(os.path.dirname(__file__), path)
+    application_path = None
+
+    # determine if application is a script file or frozen exe
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+    elif __file__:
+        application_path = os.path.dirname(__file__)
+
+    return os.path.join(application_path, path)
