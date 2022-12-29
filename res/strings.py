4f64f407fbaebedef01b75a6295808c18c781e50
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-30 01:22:05 +0200

    Move icon up one directory

--- res/strings.py
+++ res/strings.py
@@ -1,7 +1,7 @@
 from utils import path_in_current_dir, path_in_appdata_dir
 
 APP_NAME = "Quick clipboard"
-ICON_PATH = path_in_current_dir("imgs/clipboard.png")
+ICON_PATH = path_in_current_dir("clipboard.png")
 APPDATA_DIR = path_in_appdata_dir("quickclipboard")
 ADD_NEW_TEXT_DIALOG_TITE = "Add a new text"
 APP_ID = 'com.attai.youssef.quickclipboard'
