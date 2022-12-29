e765919a8ff77ac8c75c59aaa652660effb5bf03
youssef-attai <youssefgalalnazem@gmail.com>
2022-12-30 00:51:38 +0200

    Make the database filename a variable

--- res/strings.py
+++ res/strings.py
@@ -1,10 +1,7 @@
-import os
-
 from utils import path_in_current_dir, path_in_appdata_dir
 
 APP_NAME = "Quick clipboard"
 ICON_PATH = path_in_current_dir("imgs/clipboard.png")
 APPDATA_DIR = path_in_appdata_dir("quickclipboard")
-DATABASE_DIR = os.path.join(APPDATA_DIR, "texts.db")
 ADD_NEW_TEXT_DIALOG_TITE = "Add a new text"
 APP_ID = 'com.attai.youssef.quickclipboard'
